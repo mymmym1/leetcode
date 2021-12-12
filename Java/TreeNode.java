ed8d13a28903798b41acb8dd2c9d8a4e9ffd62d0
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2021-12-11 22:01:17 -0500

    Add files via upload

---
+++ Java/TreeNode.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class TreeNode {
+    Node root;
+    TreeNode() {
+        root = null;
+    }
+
+    static class Node {
+        int val;
+        Node left;
+        Node right;
+        Node() {}
+        Node(int val) { this.val = val; }
+        Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    static void printInorder(Node n) { //print in order means: left->root->right
+        if (n != null) {
+            printInorder(n.left);
+            System.out.print(n.val + ",");
+            printInorder(n.right);
+        }
+    }
+
+    public static void inorderTraversal(Node root) {
+            printInorder(root);
+    }
+
+    public static void main(String[] args) {
+        TreeNode tree = new TreeNode();
+        tree.root = new Node(1);
+        tree.root.left = null; //new Node(2);
+        tree.root.right = new Node(2);
+        //tree.root.right.left = null;//new Node(3);
+
+        inorderTraversal(tree.root);
+    }
+
+}
