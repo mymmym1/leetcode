b126a282afe5ad18a5ac03c69e033708cbf51f5d
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2021-12-17 20:40:29 -0500

    Add files via upload

---
+++ python/ListNode4.py
@@ -0,0 +1,50 @@
+# 160. Intersection of Two Linked Lists
+# Given the heads of two singly linked-lists headA and headB, return value of the node at which the two lists intersect.
+# listA and listB's length >= 1. intersectVal is 0 if listA and listB do not intersect.
+class Node:
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = next
+
+class ListNode:
+    def __init__(self):
+        self.head = None
+
+def getIntersectionNode(listnodeA, listnodeB):
+    s = set()
+    currA = listnodeA.head
+    currB = listnodeB.head
+    while currA:
+        s.add(currA)
+        currA = currA.next
+    while currB:
+        if currB in s:
+            return currB
+        currB = currB.next
+    return None
+
+llA = ListNode()
+llB = ListNode()
+'''
+llA.head = Node(4)
+llB.head = Node(5)
+intersect = Node(8, Node(4, Node(5)))
+llA.head.next = Node(1, intersect)
+llB.head.next = Node(6, Node(1, intersect))
+
+llA.head = Node(1)
+llB.head = Node(3)
+intersect = Node(2, Node(4))
+llA.head.next = Node(9, Node(1, intersect))
+llB.head.next = Node(3, intersect)
+'''
+llA.head = Node(2)
+llB.head = Node(1)
+llA.head.next = Node(6, Node(4))
+llB.head.next = Node(5)
+
+n = getIntersectionNode(llA, llB)
+if n == None:
+    print(0)
+else:
+    print("Intersected at ", n.val)
