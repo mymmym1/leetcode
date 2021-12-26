dc0e37b4019705f83dd215570cc1be25456cbd70
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2021-12-25 21:46:53 -0500

    Add files via upload

---
+++ python/CountingBits.py
@@ -0,0 +1,23 @@
+def DecimalToBinary(m):
+    temp = ""
+    while m >= 1:
+        digit = m % 2
+        temp += str(digit)
+        m = m // 2
+    binary = temp[::-1]
+    return binary
+
+def countBits(n):
+    result = list()
+    for i in range(n+1):
+        binary = DecimalToBinary(i)
+        count = 0
+        for j in range(len(binary)):
+            if binary[j] == '1':
+                count += 1
+        result.append(count)
+    return result
+
+n = 5
+print(countBits(n))
+
