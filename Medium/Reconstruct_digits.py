decfed27925fe80b18e0a6201f71245a22230e8f
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2022-01-18 11:20:55 -0500

    Update Reconstruct_digits.py

--- Medium/Reconstruct_digits.py
+++ Medium/Reconstruct_digits.py
@@ -1,70 +1,58 @@
 # digits in ascending order
 # s is guaranteed to be valid.
 
+def delc(word, s):
+    for c in word:
+        s = s.replace(c, '', 1)
+    return s
+
+def check(rl, s):
+    while 'z' in s:
+        rl.append("zero")
+        s = delc("zero", s)
+    while 'w' in s:
+        rl.append("two")
+        s = delc("two", s)
+    while 'u' in s:
+        rl.append("four")
+        s = delc("four", s)
+    while 'x' in s:
+        rl.append("six")
+        s = delc("six", s)
+    while 'g' in s:
+        rl.append("eight")
+        s = delc("eight", s)
+    while 'o' in s:
+        rl.append("one")
+        s = delc("one", s)
+    while 't' in s:
+        rl.append("three")
+        s = delc("three", s)
+    while 'f' in s:
+        rl.append("five")
+        s = delc("five", s)
+    while 's' in s:
+        rl.append("seven")
+        s = delc("seven", s)
+    while 'i' in s:
+        rl.append("nine")
+        s = delc("nine", s)
+    return s, rl
+
 def originalDigits(s):
     numberdic = {"zero":0,"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
-    resultlist = []
+    rl = []
     numlist = []
     numstr = ""
-    if 'z' in s:
-        resultlist.append("zero")
-        s = s.replace('z', '', 1)
-        s = s.replace('e', '', 1)
-        s = s.replace('r', '', 1)
-        s = s.replace('o', '', 1)
-    if 'w' in s:
-        resultlist.append("two")
-        s = s.replace('t', '', 1)
-        s = s.replace('w', '', 1)
-        s = s.replace('o', '', 1)
-    if 'u' in s:
-        resultlist.append("four")
-        s = s.replace('f', '', 1)
-        s = s.replace('o', '', 1)
-        s = s.replace('u', '', 1)
-        s = s.replace('r', '', 1)
-    if 'x' in s:
-        resultlist.append("six")
-        s = s.replace('s', '', 1)
-        s = s.replace('i', '', 1)
-        s = s.replace('x', '', 1)
-    if 'g' in s:
-        resultlist.append("eight")
-        s = s.replace('e', '', 1)
-        s = s.replace('i', '', 1)
-        s = s.replace('g', '', 1)
-        s = s.replace('h', '', 1)
-        s = s.replace('t', '', 1)
-    # Now take care of the rest
-    if 'o' in s:
-        resultlist.append("one")
-        s = s.replace('o', '', 1)
-        s = s.replace('n', '', 1)
-        s = s.replace('e', '', 1)
-    if 't' in s:
-        resultlist.append("three")
-        s = s.replace('e', '', 2)
-        s = s.replace('t', '', 1)
-        s = s.replace('r', '', 1)
-        s = s.replace('h', '', 1)
-    if 'f' in s:
-        resultlist.append("five")
-        s = s.replace('f', '', 1)
-        s = s.replace('i', '', 1)
-        s = s.replace('v', '', 1)
-        s = s.replace('e', '', 1)
-    if 's' in s:
-        resultlist.append("seven")
-        s = s.replace('e', '', 2)
-        s = s.replace('s', '', 1)
-        s = s.replace('v', '', 1)
-        s = s.replace('n', '', 1)
-    if 'i' in s:
-        resultlist.append("nine")
-        s = s.replace('e', '', 1)
-        s = s.replace('i', '', 1)
-        s = s.replace('n', '', 2)
-    for i in resultlist:
+
+    startlen = len(s)
+    s, rl = check(rl, s)
+    endlen = len(s)
+
+    if startlen == endlen and endlen != 0:
+        return s
+
+    for i in rl:
         for j in numberdic.keys():
             if i == j:
                 numlist.append(numberdic[j])
