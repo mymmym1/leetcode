8ba9973551933d0594f7034e808491fd5475680e
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2022-01-19 23:54:54 -0500

    Update FibonacciNum.py

--- python/FibonacciNum.py
+++ python/FibonacciNum.py
@@ -1,10 +1,16 @@
 def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    elif n > 1:
-        return fib(n - 1) + fib(n - 2)
+    curr, prev = 1, 1
+    for i in range(n - 2):
+        curr, prev = curr + prev, curr
+    return curr
+    
+    
+#     if n == 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     elif n > 1:
+#         return fib(n - 1) + fib(n - 2)
 
 n = 2
 n = 3
