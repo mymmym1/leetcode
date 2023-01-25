59d06e7ae9331dca71a5195ebf863a85b1e50373
Yimin Miao <39026930+mymmym1@users.noreply.github.com>
2023-01-24 22:46:53 -0500

    Update TwoSum.java
    Improve to O(n) time complexity

--- Java/TwoSum.java
+++ Java/TwoSum.java
@@ -5,14 +5,20 @@ import java.util.*;
 class TwoSum{
     public static int[] twoSum(int[] nums, int target) {
         int[] indices = new int[2];
-        for (int i=0; i<nums.length; i++) {
-            for (int j=0; j<nums.length; j++){
-                if ((i!=j) && (nums[i] + nums[j] == target)){
-                    indices[1] = i;
-                    indices[0] = j;
-                }
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int curr = nums[left] + nums[right];
+            if (curr == target) {
+                indices[0] = left;
+                indices[1] = right;            
+            }                
+            if (curr > target) {
+                right--;
+            } else {
+                left++;
             }
-        }
+        }        
         return indices;
     }
     public static void main(String[] args){
